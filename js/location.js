ea7cb62681913d4136827be642edadcc280a31e1
Violeta Josic <violetajosicc0@gmail.com>
2023-02-23 16:30:03 +0100

    META futuristic website

---
+++ js/location.js
@@ -0,0 +1,10 @@
+var audio = document.getElementsByTagName("audio");
+audio.play();
+
+var audio = $("#audio")[0];
+audio.play();
+
+var audio = $("#audio")[0];
+$("wrapper-items").mouseenter(function() {
+  audio.play();
+});
